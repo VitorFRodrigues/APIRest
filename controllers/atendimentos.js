13e0b2b60b5188e8198d16649366f5b861b646d7
Vitor Fernando de Souza Rodrigues <vitorfbaiano@gmail.com>
2021-08-01 19:06:11 -0300

    Adicionando

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,33 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realisando um GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req,res) => {
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send('Post atendimento')
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    //rota PUT usada quando ha necessidade de alteração de um objeto inteiro (mesmo que os valores sejam os mesmos)
+    //rota PATCH quando ha necessidade de alteração pontual dentro de um objeto
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        
+        Atendimento.deleta(id, res)
     })
 }
